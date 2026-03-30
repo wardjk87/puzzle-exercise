2dd41dab262d27b789783c2801cb8df102e476be
J. K. Ward <jonmichael.ward@gmail.com>
2026-03-30 10:44:33 -0600

    user types

---
+++ userParser.ts
@@ -0,0 +1,19 @@
+type UserCustomFields = {
+  readonly c1: any;
+  readonly c2: any;
+  readonly c3: any;
+}
+
+type UserType = {
+  readonly id: string;
+  readonly name: string;
+  readonly customFields: Readonly<UserCustomFields>;
+}
+
+type User = {
+  readonly email: string;
+  readonly externalId: string;
+  readonly id: string;
+  readonly name: string;
+  readonly type: Readonly<UserType>;
+}
