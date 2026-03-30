f87718d0ec1798cda9dbe4158dce2bbb4682e9de
J. K. Ward <jonmichael.ward@gmail.com>
2026-03-30 13:18:30 -0600

    cleanup of v1

--- userParser.ts
+++ userParser.ts
@@ -2,6 +2,64 @@ const TEST_INPUT = "(id, name, email, type(id, name, customFields(c1, c2, c3)), 
 
 type LevelValue = { [level: string]: {values: string[], parent: string }};
 
+// userParserV1: Returns unsorted output
+function userParserV1(input: string): string {
+  try {
+    const processedInput = prepareInputForParser(input);
+
+    let output: string = '';
+    let leadingWhiteSpaces = 0;
+    let currentValue: string = '';
+
+    for (let i = 0; i < processedInput.length; i++) {
+      const indexValue = processedInput[i];
+
+      if (indexValue === ' ') {
+        continue;
+      }
+
+      const isLastValue = i === processedInput.length - 1;
+
+      if (indexValue === ',' || isLastValue) {
+        if (isLastValue) {
+          currentValue += indexValue;
+        }
+
+        if (!currentValue) {
+          continue;
+        }
+
+        output += addNewLine(leadingWhiteSpaces, currentValue);
+        currentValue = '';
+        continue;
+      }
+
+      if (indexValue !== '(' && indexValue !== ')') {
+        currentValue += indexValue;
+        continue;
+      }
+
+      if (currentValue) {
+        output += addNewLine(leadingWhiteSpaces, currentValue);
+      }
+
+      if (indexValue === '(') {
+        leadingWhiteSpaces += 2;
+      } else {
+        leadingWhiteSpaces -= 2;
+      }
+
+      currentValue = '';
+    }
+    console.log('test1', output)
+    return output;
+  } catch (e) {
+    console.error(e);
+    return '';
+  }
+
+}
+
 function userParserWithSort(input: string): string {
   if (typeof input !== "string"){
     throw new Error('Input is not a valid string')
@@ -127,71 +185,7 @@ function setLevelValue(levelValue: LevelValue, currentLevel: number, currentValu
   return updatedLevelValue;
 }
 
-function userParser(input: string): string {
-  if (typeof input !== "string"){
-    throw new Error('Input is not a valid string')
-  }
-  const inputTrimmed = input.trim()
 
-  if (inputTrimmed[0] !== '(' || inputTrimmed[inputTrimmed.length -1] !== ')') {
-    throw new Error('Input is not a valid parenthetical value');
-  }
-
-  const commaDelimitedValuesStringified = removeRootParenthesis(inputTrimmed)
-
-  let output: string = '';
-  let leadingWhiteSpaces = 0;
-  let currentValue: string = '';
-
-  for (let i = 0; i < commaDelimitedValuesStringified.length; i++) {
-    const indexValue = commaDelimitedValuesStringified[i];
-
-    if (indexValue === ' ') {
-      continue;
-    }
-
-    const lastValue = i === commaDelimitedValuesStringified.length - 1;
-
-    if (indexValue === ',' || lastValue) {
-      if (lastValue) {
-        currentValue += indexValue;
-      }
-
-      if (!currentValue) {
-        continue;
-      }
-
-      output += addNewLine(leadingWhiteSpaces, currentValue);
-      currentValue = '';
-      continue;
-    }
-
-    if (indexValue !== '(' && indexValue !== ')') {
-      currentValue += indexValue;
-      continue;
-    }
-
-    if (indexValue === '(') {
-      output += addNewLine(leadingWhiteSpaces, currentValue);
-      leadingWhiteSpaces += 2;
-
-      currentValue = '';
-      continue;
-    }
-
-    if (indexValue === ')') {
-      if (currentValue) {
-        console.log('currentValue', currentValue, leadingWhiteSpaces)
-        output += addNewLine(leadingWhiteSpaces, currentValue);
-      }
-
-      leadingWhiteSpaces -= 2;
-      currentValue = '';
-      continue;
-    }
-  }
-  return output;
-}
 
 function addNewLine(leadingWhiteSpaces: number, currentValue: string) {
   if (leadingWhiteSpaces) {
@@ -206,7 +200,20 @@ function removeRootParenthesis(input: string): string {
   return input.substring(1, input.length - 1);
 }
 
-userParserWithSort(TEST_INPUT);
+function prepareInputForParser(input: string): string {
+  if (typeof input !== "string"){
+    throw new Error('Input is not a valid string')
+  }
+  const inputTrimmed = input.trim()
+
+  if (inputTrimmed[0] !== '(' || inputTrimmed[inputTrimmed.length -1] !== ')') {
+    throw new Error('Input is not a valid parenthetical value');
+  }
+
+  return removeRootParenthesis(inputTrimmed)
+}
+
+// userParserWithSort(TEST_INPUT);
 
 
-// userParser(TEST_INPUT);
+userParserV1(TEST_INPUT);
