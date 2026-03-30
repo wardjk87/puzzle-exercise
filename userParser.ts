6ea1a1e31f5726294106c86d382b418ce882ae6c
J. K. Ward <jonmichael.ward@gmail.com>
2026-03-30 11:40:19 -0600

    progress in parser to build output based on iterating over each index of string

--- userParser.ts
+++ userParser.ts
@@ -1,5 +1,5 @@
 const TEST_INPUT = "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
-
+const PARENTHESIS = ['(', ')']
 function userParser(input: string): string {
   if (typeof input !== "string"){
     throw new Error('Input is not a valid string')
@@ -10,10 +10,68 @@ function userParser(input: string): string {
     throw new Error('Input is not a valid parenthetical value');
   }
 
-  const CommaDelimitedValuesStringified = removeRootParenthesis(inputTrimmed)
-  console.log('CommaDelimitedValuesStringified', CommaDelimitedValuesStringified)
-  const splitValues = splitInputByComma(CommaDelimitedValuesStringified);
+
+  const commaDelimitedValuesStringified = removeRootParenthesis(inputTrimmed)
+
+  const split = commaDelimitedValuesStringified.split('(');
+  const output: string = '';
+  console.log('CommaDelimitedValuesStringified', commaDelimitedValuesStringified)
+  console.log('split', split)
+  const parentValues: string[] = [];
+  const output: string = '';
+  let leadingWhiteSpaces = 0;
+
+  let currentValue: string = '';
+  for (let i = 0; i < commaDelimitedValuesStringified.length; i++) {
+    const indexValue = commaDelimitedValuesStringified[i];
+
+    if (!indexValue) {
+      continue;
+    }
+
+    if (indexValue === ',') {
+      if (leadingWhiteSpaces) {
+        parentValues.push('\n' + (new Array(leadingWhiteSpaces).join(' ')) + ` - ${currentValue}`);
+        currentValue = '';
+      } else {
+        parentValues.push(`\n- ${currentValue}`);
+        currentValue = '';
+      }
+
+      continue;
+    }
+
+    if (indexValue !== '(' && indexValue !== ')') {
+      currentValue.concat(indexValue);
+    }
+
+    if (indexValue === '(') {
+      leadingWhiteSpaces += 2;
+      parentValues.push(`\n- ${currentValue}`);
+      currentValue = '';
+      continue;
+    }
+
+    if (indexValue === ')') {
+      leadingWhiteSpaces -= 2;
+      parentValues.push(`\n- ${currentValue}`);
+      currentValue = '';
+      continue;
+    }
+
+    currentValue += indexValue;
+  }
+
+  console.log('parentValues', parentValues);
+
+  const splitValues = splitInputByComma(commaDelimitedValuesStringified);
   console.log('splitValues', splitValues)
+
+  // for (let i = 0; i < splitValues.length; i++) {
+  //   const indexValue = splitValues[i];
+  //   if (indexValue.includes('(') || in)
+  //   splitValues[i];
+  // }
   return splitValues.toString();
 }
 
